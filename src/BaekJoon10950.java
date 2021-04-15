e91cd78a19c2d4666a12973d14accca3e65a0000
이승민 <sml7179@naver.com>
2021-04-15 20:45:46 +0900

    BaekJoon10950 file Add

---
+++ src/BaekJoon10950.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BaekJoon10950 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<N;i++){
+            String [] input=br.readLine().split(" ");
+           int input1=Integer.parseInt(input[0]);
+           int input2=Integer.parseInt(input[1]);
+           sb.append(input1+input2+"\n");
+        }
+        System.out.println(sb);
+    }
+}
