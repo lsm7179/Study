007e8cfe79ec9d61d8e2243f2a9f2b411b2872de
이승민 <sml7179@naver.com>
2021-05-07 16:56:58 +0900

    BaekJoon2439 fila add

---
+++ src/BaekJoon2439.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BaekJoon2439 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        int number=Integer.parseInt(br.readLine());
+        for(int i=0;i<number;i++){
+
+            for(int j=(number-1);j>i;j--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
