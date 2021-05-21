f03a4a5690df4726be331d044754489954431024
이승민 <sml7179@naver.com>
2021-05-21 18:03:24 +0900

    프로그래머스 코드챌린지 다시 도전중

--- src/Programmers/MonthlyCodeChallengeSeason2_2_2.java
+++ src/Programmers/MonthlyCodeChallengeSeason2_2_2.java
@@ -34,4 +34,24 @@ public class MonthlyCodeChallengeSeason2_2_2 {
         }
         return bit;
     }
+
+    public static Long check(long number){
+        StringBuilder binaryString=new StringBuilder(Long.toBinaryString(number)); //test = 10->1010->1011 -> 11 //test = 9-> 1001 -> 1010 -> 10
+
+        if(number%2==0){//짝수 제일 낮은 0을 1로 바꾸면 끝
+            int indexZero=binaryString.lastIndexOf("0");
+            binaryString.setCharAt(indexZero,'1');
+            //System.out.println(binaryString);
+        }else{//홀수
+            /* 제일 낮은 0을 1로 바꾸고
+               그 보다 낮으면서 제일 높은 자리의 1을 0으로 바꿔야함.*/
+            int indexZero=binaryString.lastIndexOf("0");
+            int indexOne=binaryString.lastIndexOf("1",indexZero+1);
+            binaryString.setCharAt(indexZero,'1');
+            binaryString.setCharAt(indexOne,'0');
+            //System.out.println(binaryString);
+        }
+        return Long.parseLong(binaryString.toString(),2);
+    }
+
 }
