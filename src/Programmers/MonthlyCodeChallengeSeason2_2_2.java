5cf54f2135bf00e1907d8f78b16ffeb824046861
이승민 <sml7179@naver.com>
2021-05-22 16:16:19 +0900

    프로그머스 코드챌린지 2개 이하로 다른 비트 문제풀이

--- src/Programmers/MonthlyCodeChallengeSeason2_2_2.java
+++ src/Programmers/MonthlyCodeChallengeSeason2_2_2.java
@@ -3,55 +3,38 @@ package Programmers;
 public class MonthlyCodeChallengeSeason2_2_2 {
 
     public static void main(String[] args) {
-        long [] numbers={2,7};
-        System.out.println(solution(numbers));
-
+        long [] numbers={2,7,13,3,9};
+        // return 값 3,11,14,5,10
+        solution(numbers);
     }
 
     public static long[] solution(long[] numbers) {
         long[] answer = new long[numbers.length];
         for(int i=0;i<numbers.length;i++){
-            answer[i]=bitChk(numbers[i],++numbers[i]);
+            answer[i]=check(numbers[i]);
             System.out.println(answer[i]);
         }
         return answer;
     }
 
-    public static long bitChk(long ori, long bit){
-        double doubleBit=ori^bit;
-        long test=Double.doubleToRawLongBits(doubleBit);
-        String strResult=Long.toBinaryString(test);
-        //bit에서 1이 2개이상있다는건
-
-        int chk=0;
-        for(int i=0;i<strResult.length();i++){
-            if('1'==strResult.charAt(i)){
-                chk++;
-            }
-            if(chk>2){
-                return bitChk(ori,bit+=1);
-            }
-        }
-        return bit;
-    }
-
     public static Long check(long number){
-        StringBuilder binaryString=new StringBuilder(Long.toBinaryString(number)); //test = 10->1010->1011 -> 11 //test = 9-> 1001 -> 1010 -> 10
-
-        if(number%2==0){//짝수 제일 낮은 0을 1로 바꾸면 끝
-            int indexZero=binaryString.lastIndexOf("0");
-            binaryString.setCharAt(indexZero,'1');
-            //System.out.println(binaryString);
+        long result=number;
+        if(number%2==0){//짝수 제일 낮은 0을 1로 바꾸면 끝 짝수는 무조건 제일 낮은 0이 맨 끝일수밖에없음
+            result+=1;
         }else{//홀수
             /* 제일 낮은 0을 1로 바꾸고
                그 보다 낮으면서 제일 높은 자리의 1을 0으로 바꿔야함.*/
-            int indexZero=binaryString.lastIndexOf("0");
-            int indexOne=binaryString.lastIndexOf("1",indexZero+1);
-            binaryString.setCharAt(indexZero,'1');
-            binaryString.setCharAt(indexOne,'0');
-            //System.out.println(binaryString);
+            int count=0;
+            while(number!=0){
+                count++;
+                if(number%2==0) break;
+                number=number/2;
+            }
+            if(number!=0)
+            result+=(Math.pow(2,count-1)-Math.pow(2,count-2));
+            else result+=(Math.pow(2,count)-Math.pow(2,count-1));
         }
-        return Long.parseLong(binaryString.toString(),2);
+        return result;
     }
 
 }
