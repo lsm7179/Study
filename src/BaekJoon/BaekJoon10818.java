811f0fa3780f445f19106858c114dc6a68dc840f
이승민 <sml7179@naver.com>
2021-06-21 16:15:35 +0900

    백준 10818 최소,최대 문제풀이

---
+++ src/BaekJoon/BaekJoon10818.java
@@ -0,0 +1,23 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon10818 {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb=new StringBuilder();
+        int testCase=Integer.parseInt(br.readLine());
+        StringTokenizer strTok=new StringTokenizer(br.readLine()," ");
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<testCase;i++){
+           int number=Integer.parseInt(strTok.nextToken());
+           min=Integer.min(number,min);
+           max=Integer.max(number,max);
+        }
+        System.out.print(min+" "+max);
+    }
+}
