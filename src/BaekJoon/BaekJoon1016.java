e7992abff937bf1e62f26ac63d46c1c0256bbffa
이승민 <sml7179@naver.com>
2021-06-21 16:05:04 +0900

    백준 1016 제곱 ㄴㄴ 수 문제풀이

--- src/BaekJoon/BaekJoon1016.java
+++ src/BaekJoon/BaekJoon1016.java
@@ -8,7 +8,23 @@ public class BaekJoon1016 {
 
     public static void main(String[] args) throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer=new StringTokenizer(br.readLine(), " ");
+        long min=Long.parseLong(stringTokenizer.nextToken());
+        long max=Long.parseLong(stringTokenizer.nextToken());
+        int size=(int)(max-min+1);
+        boolean [] isSqrt=new boolean[size];
 
+        for(long i=2;i*i<=max;i++){
+            long pow=i*i;
+            long start=min % pow == 0 ? min /pow : (min/pow)+1;
+            for(long j = start; pow * j <= max; j++){
+                if(!isSqrt[(int)((j * pow) - min)]){
+                    isSqrt[(int)((j * pow) - min)] = true;
+                    size--;
+                }
 
+            }
+        }
+        System.out.print(size);
     }
 }
