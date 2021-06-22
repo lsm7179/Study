afd611d7109c9690d93a3462f8df2d715dc59b09
이승민 <sml7179@naver.com>
2021-06-22 14:29:33 +0900

    백준 10809 알파벳 찾기 문제풀이

---
+++ src/BaekJoon/BaekJoon10809.java
@@ -0,0 +1,19 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BaekJoon10809 {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String test= br.readLine();
+        int []arr=new int[26];//97
+        for(int i=0;i<test.length();i++){
+            if(arr[test.charAt(i)-97]==0)
+            arr[test.charAt(i)-97]=i+1;
+        }
+        Arrays.stream(arr).forEach(x-> System.out.print((x-1)+" "));
+    }
+}
