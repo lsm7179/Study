605c032aa2f62406d6c9d10f4349459b0f9f38ab
이승민 <sml7179@naver.com>
2021-06-22 13:55:33 +0900

    백준 3052 나머지 문제풀이

---
+++ src/BaekJoon/BaekJoon3052.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BaekJoon3052 {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        boolean []chk=new boolean[42];
+        int result=0;
+        for(int i=0;i<10;i++){
+            chk[Integer.parseInt(br.readLine())%42]=true;
+        }
+        for(int i=0;i<chk.length;i++){
+            if(chk[i]) result++;
+        }
+        System.out.print(result);
+    }
+}
