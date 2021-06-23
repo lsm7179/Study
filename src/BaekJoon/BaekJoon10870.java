34ac76ea60a9f6a19aa1a41f07080f062f91f9c9
이승민 <sml7179@naver.com>
2021-06-23 17:37:37 +0900

    백준 10870 피보나치 수 5 문제풀이

---
+++ src/BaekJoon/BaekJoon10870.java
@@ -0,0 +1,25 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BaekJoon10870 {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        each(0,1,n);
+
+    }
+
+    public static void each(int i,int j,int n){
+        if(n==0){
+            System.out.print(i);
+            System.exit(0);
+        }
+        n--;
+        each(j,i+j,n);
+
+    }
+
+}
