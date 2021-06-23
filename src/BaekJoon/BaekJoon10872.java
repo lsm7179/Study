1b43ca0637dec92614f0fae3b276b6ba08078f46
이승민 <sml7179@naver.com>
2021-06-23 17:22:06 +0900

    백준 10872 팩토리얼 문제풀이

---
+++ src/BaekJoon/BaekJoon10872.java
@@ -0,0 +1,19 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BaekJoon10872 {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        int tot=1;
+        for(int i=1;i<=n;i++){
+            tot*=(i);
+        }
+        System.out.print(tot);
+
+    }
+
+}
