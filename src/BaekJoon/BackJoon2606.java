307c6c956f126c225d4bb6db94a84230328744ae
이승민 <sml7179@naver.com>
2021-07-12 20:49:48 +0900

    백준 2606 바이러스 문제풀이

--- src/BaekJoon/BackJoon2606.java
+++ src/BaekJoon/BackJoon2606.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class BackJoon2606 {
     static int node[][]; // 그래프 배열
-    static int check[]; // 방문 배열
+    static boolean isVisited[]; // 방문 배열
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -15,7 +15,7 @@ public class BackJoon2606 {
         int m = sc.nextInt(); // 네트워크 상에 연결되어 있는 컴퓨터 쌍의 수 즉, 간선의 수
 
         node=new int[n+1][n+1];
-        check = new int[n+1];
+        isVisited = new boolean[n+1];
         for(int i=0;i<m;i++) { // 그래프 구성
             int a=sc.nextInt();
             int b = sc.nextInt();
@@ -31,16 +31,16 @@ public class BackJoon2606 {
     static void bfs() { // BFS 메소드
         Queue<Integer> queue = new LinkedList<>();
 
-        check[1] =1;
+        isVisited[1] =true;
         queue.offer(1);
         int cnt = 0; // 감염 된 컴퓨터의 수
         while(!queue.isEmpty()) {
             int x = queue.poll();
 
             for(int i=1;i<node.length;i++) { // 차례대로 1번과 연결 된 컴퓨터들을 찾아 cnt변수 증가
-                if(node[x][i]==1 && check[i]!=1) {
+                if(node[x][i]==1 && !isVisited[i]) {
                     queue.offer(i);
-                    check[i] = 1;
+                    isVisited[i] = true;
                     cnt++;
                 }
             }
