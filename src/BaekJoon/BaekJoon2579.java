bbec14a844dea13f3185b94971e08b8d541b5f97
이승민 <sml7179@naver.com>
2021-07-14 10:22:34 +0900

    refactor 계단오르기 리팩토링

--- src/BaekJoon/BaekJoon2579.java
+++ src/BaekJoon/BaekJoon2579.java
@@ -5,25 +5,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BaekJoon2579 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        int f[] = new int[T+1];
-        int cache[] = new int[T+1];
-        for(int i = 1; i<=T; i++){
-            f[i] = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int[] stairs = new int[n + 1];
+        int[] maxScore = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            stairs[i] = Integer.parseInt(br.readLine());
         }
-        if(T>=3) {
-            cache[1] = f[1];
-            cache[2] = f[2] + f[1];
-            cache[3] = Math.max(f[1] + f[3], f[2] + f[3]);
-            for (int i = 4; i <= T; i++) {
-                cache[i] = Math.max(cache[i - 3] + f[i] + f[i - 1], cache[i - 2] + f[i]);
-            }
-            System.out.println(cache[T]);
-        }else{
-            if(T==1) System.out.println(f[1]);
-            if(T==2) System.out.println(f[1]+f[2]);
+        if (n >= 1)
+            maxScore[1] = stairs[1];
+        if (n >= 2)
+            maxScore[2] = stairs[1] + stairs[2];
+        if (n >= 3)
+            maxScore[3] = Integer.max(stairs[1] + stairs[3], stairs[2] + stairs[3]);
+        for (int i = 4; i <= n; i++) {
+            maxScore[i] = Integer.max(maxScore[i - 3] + stairs[i - 1] + stairs[i], maxScore[i - 2] + stairs[i]);
         }
+        System.out.print(maxScore[n]);
     }
 }
