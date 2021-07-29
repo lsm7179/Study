b916a14b4ea456da4e2a4cd8cfc26cae85947e5c
이승민 <sml7179@naver.com>
2021-07-29 22:24:12 +0900

    백준 1149 RGB거리 문제풀이 코드 수정

--- src/BaekJoon/BaekJoon1149.java
+++ src/BaekJoon/BaekJoon1149.java
@@ -3,22 +3,26 @@ package BaekJoon;
 import java.util.Scanner;
 
 public class BaekJoon1149 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int i, j, n = sc.nextInt();
-        int d[][] = new int[n+1][3];
-        int a[][] = new int[n+1][3];
-        for(i=1;i<=n;i++)
-            for(j=0;j<3;j++)
-                a[i][j] = sc.nextInt();
-        d[0][0] = d[0][1] = d[0][2] = a[0][0] = a[0][1] = a[0][2] = 0;
-        for(i=1;i<=n;i++){
-            d[i][0] = Min(d[i-1][1], d[i-1][2]) + a[i][0];
-            d[i][1] = Min(d[i-1][0], d[i-1][2]) + a[i][1];
-            d[i][2] = Min(d[i-1][0], d[i-1][1]) + a[i][2];
+    public static void main(String[] args) throws Exception {
+        Scanner scanner=new Scanner(System.in);
+        int n = scanner.nextInt();
+        int color[][]=new int[3][n+1];
+        int result[][]=new int[3][n+1];
+
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<3;j++){
+                color[j][i]=scanner.nextInt();
+            }
+        }// 초기값 세팅
+
+        result[0][0]=result[1][0]=result[2][0]=color[0][0]=color[1][0]=color[2][0]=0;
+        for(int i=1;i<=n;i++){
+            result[0][i]=Integer.min(result[1][i-1], result[2][i-1]) + color[0][i];
+            result[1][i]=Integer.min(result[0][i-1], result[2][i-1]) + color[1][i];
+            result[2][i]=Integer.min(result[0][i-1], result[1][i-1]) + color[2][i];
         }
-        System.out.println(Min(Min(d[n][0], d[n][1]), d[n][2]));
-        sc.close();
+        System.out.println(Integer.min(result[0][n],Integer.min(result[1][n],result[2][n])));
+        scanner.close();
     }
-    public static int Min(int a, int b){ return a < b ? a : b; }
+
 }
