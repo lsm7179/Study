b51758763a87d7cef70fdcf6df9d9aaee6305af3
이승민 <sml7179@naver.com>
2021-08-07 19:59:46 +0900

    백준 9466 텀프로젝트 문제풀이

---
+++ src/BaekJoon/BaekJoon9466.java
@@ -0,0 +1,15 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BaekJoon9466 {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        
+
+    }
+
+}
+
