05d3b3ff4e0a31d3f1f8e98f6a9de8c195a65079
이승민 <sml7179@naver.com>
2021-08-10 16:53:58 +0900

    프로그래머스 상호 평가 문제풀이

--- src/Programmers/Solution_83201.java
+++ src/Programmers/Solution_83201.java
@@ -3,15 +3,36 @@ package Programmers;
 //https://programmers.co.kr/learn/courses/30/lessons/83201
 public class Solution_83201 {
     public static void main(String[] args) {
-        int [][]scores={{100,90,98,88,65},{50,45,99,85,77},{47,88,95,80,67},{61,57,100,80,65},{24,90,94,75,65}};
+        //[[75, 50, 100], [75, 100, 20], [100, 100, 20]] -> BBF
+        int [][]scores={{75,50,100},{75,100,20},{100, 100, 20}};
         System.out.println(solution(scores));
     }
     public static String solution(int[][] scores) {
+        StringBuilder answer = new StringBuilder();
+        for(int i=0;i<scores.length;i++){
+            int max=scores[i][i];
+            int min=scores[i][i];
+            int tot=0;
+            boolean unique=true;
+            for(int j=0;j<scores.length;j++){
+                if(i!=j&&unique){
+                    if(scores[i][i]==scores[j][i]) unique=false;
+                    max=Integer.max(max,scores[j][i]);
+                    min=Integer.min(min,scores[j][i]);
+                }
+                tot+=scores[j][i];
+            }
+            if(scores[i][i]==max&&unique){
+                answer.append(getGrade((tot-max)/(scores.length-1)));
+            }else if(scores[i][i]==min&&unique){
+                answer.append(getGrade((tot-min)/(scores.length-1)));
+            }else{
+                answer.append(getGrade((tot)/(scores.length)));
 
-        System.out.println(scores.length);
+            }
+        }
 
-        String answer = "";
-        return answer;
+        return answer.toString();
     }
 
     public static String getGrade(double score){
@@ -20,9 +41,9 @@ public class Solution_83201 {
         }else if(score>=80){
             return "B";
         }else if(score>=70){
-            return "B";
-        }else if(score>=60){
-            return "B";
+            return "C";
+        }else if(score>=50){
+            return "D";
         }else{
             return "F";
         }
