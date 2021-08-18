ce2583c2f5d08a2983ed2a30733a280c5624df8e
이승민 <sml7179@naver.com>
2021-08-18 20:17:44 +0900

    백준 11286 절댓값 힙 문제풀이

---
+++ src/BaekJoon/BaekJoon11286.java
@@ -0,0 +1,41 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class BaekJoon11286 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> priorityQueue=new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if(Math.abs(o1)-Math.abs(o2)==0){
+                    return o1-o2;
+                }else{//절댓값이 가장 작은 값을 출력 절댓값이 같다면 -부터 출력
+                    return Math.abs(o1)-Math.abs(o2);
+                }
+
+            }
+        });
+        StringBuffer sb=new StringBuffer();
+        for(int i=0;i<N;i++){
+            int input=Integer.parseInt(br.readLine());
+            if(input==0){
+                if(priorityQueue.isEmpty()){
+                    sb.append(0+"\n");
+                }else{
+                    sb.append(priorityQueue.poll()+"\n");
+
+                }
+            }else{
+                priorityQueue.add(input);
+            }
+        }
+        System.out.print(sb.toString());
+
+    }
+
+}
