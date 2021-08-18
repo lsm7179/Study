fc4870f7a92292c6bf2549dbebf77312cfce2857
이승민 <sml7179@naver.com>
2021-08-18 20:09:06 +0900

    백준 11279 최대 힙 문제풀이

---
+++ src/BaekJoon/BaekJoon11279.java
@@ -0,0 +1,33 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class BaekJoon11279 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> priorityQueue=new PriorityQueue<>(Collections.reverseOrder());
+        StringBuffer sb=new StringBuffer();
+        for(int i=0;i<N;i++){
+            int input=Integer.parseInt(br.readLine());
+            if(input==0){
+                if(priorityQueue.isEmpty()){
+                    sb.append(0+"\n");
+                }else{
+                    sb.append(priorityQueue.poll()+"\n");
+
+                }
+            }else{
+                priorityQueue.add(input);
+            }
+        }
+        System.out.print(sb.toString());
+
+    }
+
+
+
+}
