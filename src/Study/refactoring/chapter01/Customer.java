001e62983e1551b5c9486ac16ca72451e6a7325e
이승민 <sml7179@naver.com>
2021-08-21 11:42:56 +0900

    리팩토링 첫 단계  임시변수를 메서드 호출로 전환  적립 포인트 계산을 메서드로 빼기

--- src/Study/refactoring/chapter01/Customer.java
+++ src/Study/refactoring/chapter01/Customer.java
@@ -5,12 +5,13 @@ import java.util.Vector;
 
 class Customer {
     private String name;
-    private Vector _rentals =new Vector();
+    private Vector _rentals = new Vector();
 
     public Customer(String name) {
         this.name = name;
     }
-    public void addRental(Rental arg){
+
+    public void addRental(Rental arg) {
         _rentals.addElement(arg);
     }
 
@@ -18,33 +19,26 @@ class Customer {
         return name;
     }
 
-    public String statement(){
+    public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration rentals=_rentals.elements();
-        String result=getName() + " 고객님의 대여 기록\n";
-        while(rentals.hasMoreElements()){
-            double thisAmount = 0;
-            Rental each=(Rental) rentals.nextElement();
+        Enumeration rentals = _rentals.elements();
+        String result = getName() + " 고객님의 대여 기록\n";
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
 
-            thisAmount = each.getCharge();
-
-            //적립 포인트를 1 포인트 증가
-            frequentRenterPoints ++;
-            //최신물을 이틀 이상 대여하면 보너스 포인트 지급
-            if((each.getMovie().getPriceCode()==Movie.NEW_RELEASE)&&
-                    each.getDaysRented()>1) frequentRenterPoints++;
+            frequentRenterPoints += each.getFrequentRenterPoints();
 
             //이번에 대여하는 비디오 정보와 대여료를 출력
-            result += "\t" + each.getMovie().getTitle()+"\t" +
-                    String.valueOf(thisAmount) + "\n";
+            result += "\t" + each.getMovie().getTitle() + "\t" +
+                    String.valueOf(each.getCharge()) + "\n";
 
             //현재까지 누적된 총 대여료
-            totalAmount +=thisAmount;
+            totalAmount += each.getCharge();
         }
         //푸터 행 추가
-        result += "누적 대여료 "+ String.valueOf(totalAmount) +"\n";
-        result += "적립 포인트 "+ String.valueOf(frequentRenterPoints) +"\n";
+        result += "누적 대여료 " + String.valueOf(totalAmount) + "\n";
+        result += "적립 포인트 " + String.valueOf(frequentRenterPoints) + "\n";
         return result;
     }
 
--- src/Study/refactoring/chapter01/Rental.java
+++ src/Study/refactoring/chapter01/Rental.java
@@ -20,21 +20,29 @@ class Rental {
     //비디오 종류별 대여료 계산
     double getCharge() {
         double result = 0;
-        switch (getMovie().getPriceCode()){
+        switch (getMovie().getPriceCode()) {
             case Movie.REAGULAR:
                 result += 2;
-                if(getDaysRented()>2)
-                    result +=(getDaysRented()-2) * 1.5;
+                if (getDaysRented() > 2)
+                    result += (getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                result += getDaysRented()*3;
+                result += getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
-                result +=1.5;
-                if(getDaysRented()>3)
-                    result +=(getDaysRented()-3)*1.5;
+                result += 1.5;
+                if (getDaysRented() > 3)
+                    result += (getDaysRented() - 3) * 1.5;
                 break;
         }
         return result;
+    }
+
+    //최신물을 이틀 이상 대여하면 2포인트 지급하고 그외엔 1포인트 지급하는 코드를
+    //빼내 getFrequentRenterPoints 메서드로 만들고 이 Rental 클래스로 옮겼다.
+    public int getFrequentRenterPoints() {
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+                getDaysRented() > 1) return 2;
+        else return 1;
     }
 }
