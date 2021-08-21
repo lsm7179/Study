b6c81e5936147731a510f7cb8fa76234e4191690
이승민 <sml7179@naver.com>
2021-08-21 11:26:27 +0900

    리팩토링 첫 단계   메서드 옮기기

--- src/Study/refactoring/chapter01/Customer.java
+++ src/Study/refactoring/chapter01/Customer.java
@@ -27,7 +27,7 @@ class Customer {
             double thisAmount = 0;
             Rental each=(Rental) rentals.nextElement();
 
-            thisAmount = amountFor(each);
+            thisAmount = each.getCharge();
 
             //적립 포인트를 1 포인트 증가
             frequentRenterPoints ++;
@@ -48,24 +48,4 @@ class Customer {
         return result;
     }
 
-    //비디오 종류별 대여료 계산
-    private double amountFor(Rental aRental) {
-        double result = 0;
-        switch (aRental.getMovie().getPriceCode()){
-            case Movie.REAGULAR:
-                result += 2;
-                if(aRental.getDaysRented()>2)
-                    result +=(aRental.getDaysRented()-2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented()*3;
-                break;
-            case Movie.CHILDRENS:
-                result +=1.5;
-                if(aRental.getDaysRented()>3)
-                    result +=(aRental.getDaysRented()-3)*1.5;
-                break;
-        }
-        return result;
-    }
 }
--- src/Study/refactoring/chapter01/Rental.java
+++ src/Study/refactoring/chapter01/Rental.java
@@ -16,4 +16,25 @@ class Rental {
     public int getDaysRented() {
         return daysRented;
     }
+
+    //비디오 종류별 대여료 계산
+    double getCharge() {
+        double result = 0;
+        switch (getMovie().getPriceCode()){
+            case Movie.REAGULAR:
+                result += 2;
+                if(getDaysRented()>2)
+                    result +=(getDaysRented()-2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += getDaysRented()*3;
+                break;
+            case Movie.CHILDRENS:
+                result +=1.5;
+                if(getDaysRented()>3)
+                    result +=(getDaysRented()-3)*1.5;
+                break;
+        }
+        return result;
+    }
 }
