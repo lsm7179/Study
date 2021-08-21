1fe857282d4166eec4258fdf414ac09bb2acbf9a
이승민 <sml7179@naver.com>
2021-08-21 13:00:25 +0900

    리팩토링 두 번째  가격 책정 부분의 조건문을 재정의로 교체

--- src/Study/refactoring/chapter01/Movie.java
+++ src/Study/refactoring/chapter01/Movie.java
@@ -24,4 +24,25 @@ public class Movie {
     public void setPriceCode(int priceCode) {
         this.priceCode = priceCode;
     }
+
+    //비디오 종류별 대여료 계산
+    double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REAGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
 }
