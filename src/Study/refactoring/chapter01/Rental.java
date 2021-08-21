4118a5f66d6d4fa9c7ef8bc00cd7d7a17341a2e1
이승민 <sml7179@naver.com>
2021-08-21 12:16:39 +0900

    리팩토링 첫 단계  임시변수 없애기(totalAmount, frequentRenterPoints)

--- src/Study/refactoring/chapter01/Rental.java
+++ src/Study/refactoring/chapter01/Rental.java
@@ -38,7 +38,7 @@ class Rental {
         return result;
     }
 
-    //최신물을 이틀 이상 대여하면 2포인트 지급하고 그외엔 1포인트 지급하는 코드를
+    //최신물을 이틀 이상 대여하면 2포인트 지급하고 그외엔 1포인 트 지급하는 코드를
     //빼내 getFrequentRenterPoints 메서드로 만들고 이 Rental 클래스로 옮겼다.
     public int getFrequentRenterPoints() {
         if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
