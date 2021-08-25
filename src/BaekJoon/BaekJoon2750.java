bac7c311df22806a98be08349cd695d439f53495
이승민 <sml7179@naver.com>
2021-08-25 20:28:17 +0900

    거품정렬 구현

--- src/BaekJoon/BaekJoon2750.java
+++ src/BaekJoon/BaekJoon2750.java
@@ -5,27 +5,41 @@ import java.io.InputStreamReader;
 
 public class BaekJoon2750 {
     static int[] array;
-    public static void main(String[] args) throws Exception{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int N=Integer.parseInt(br.readLine());
-        array=new int[N];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        array = new int[N];
         for (int i = 0; i < array.length; i++) {
-            array[i]=Integer.parseInt(br.readLine());
+            array[i] = Integer.parseInt(br.readLine());
         }
-        selectionSort();
+        bubbleSort();
         printArray();
     }
-    private static void selectionSort() {
-        for (int i = 0; i < array.length-1; i++) {
-            int minIndex=i;
-            for (int j = i; j < array.length; j++) {
-                if(array[minIndex]>array[j]){
-                    minIndex=j;
+
+    public static void bubbleSort() {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
-            int temp=array[minIndex];
-            array[minIndex]=array[i];
-            array[i]=temp;
+        }
+    }
+
+    private static void selectionSort() {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i; j < array.length; j++) {
+                if (array[minIndex] > array[j]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
         }
     }
 
@@ -34,8 +48,6 @@ public class BaekJoon2750 {
             System.out.println(i);
         }
     }
-
-
 
 
 }
--- src/Study/sort/Sort.java
+++ src/Study/sort/Sort.java
@@ -1,30 +1,45 @@
 package Study.sort;
 
 public class Sort {
-    static int[] array = {8,5,2,6,3,7,9,1,4};
+    static int[] array = {8, 5, 2, 6, 3, 7, 9, 1, 4};
+
     public static void main(String[] args) {
-        selectionSort();
+        //selectionSort();
+        bubbleSort();
         printArray();
     }
 
-    //선택정렬
-    public static void selectionSort(){
-        for (int i = 0; i < array.length-1; i++) {
-            int indexMin=i;
-            for (int j = i+1; j < array.length; j++) {
-                if(array[indexMin]>array[j]){
-                    indexMin=j;
+    //거품정렬
+    public static void bubbleSort() {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
-            int temp=array[i];
-            array[i]=array[indexMin];
-            array[indexMin]=temp;
         }
     }
 
-    public static void printArray(){
+    //선택정렬
+    public static void selectionSort() {
+        for (int i = 0; i < array.length - 1; i++) {
+            int indexMin = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[indexMin] > array[j]) {
+                    indexMin = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[indexMin];
+            array[indexMin] = temp;
+        }
+    }
+
+    public static void printArray() {
         for (int i : array) {
-            System.out.print(i+" ");
+            System.out.print(i + " ");
         }
         System.out.println();
     }
