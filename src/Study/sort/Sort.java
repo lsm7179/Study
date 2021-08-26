0607085d8f485bb4b3b1d9dd6a38061ad2efece8
이승민 <sml7179@naver.com>
2021-08-26 17:45:37 +0900

    update 정렬알고리즘.md
    삽입정렬 추가

--- src/Study/sort/Sort.java
+++ src/Study/sort/Sort.java
@@ -1,22 +1,35 @@
 package Study.sort;
 
 public class Sort {
-    static int[] array = {8, 5, 2, 6, 3, 7, 9, 1, 4};
+    static int[] arr = {8, 5, 2, 6, 3, 7, 9, 1, 4};
 
     public static void main(String[] args) {
         //selectionSort();
-        bubbleSort();
+        //bubbleSort();
+        insertionSort();
         printArray();
+    }
+
+    public static void insertionSort() {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int aux = i - 1;
+            while (aux >= 0 && (arr[aux] > temp)) {
+                arr[aux + 1] = arr[aux];
+                aux--;
+            }
+            arr[aux + 1] = temp;
+        }
     }
 
     //거품정렬
     public static void bubbleSort() {
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 0; j < array.length - i - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
@@ -24,21 +37,21 @@ public class Sort {
 
     //선택정렬
     public static void selectionSort() {
-        for (int i = 0; i < array.length - 1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             int indexMin = i;
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[indexMin] > array[j]) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[indexMin] > arr[j]) {
                     indexMin = j;
                 }
             }
-            int temp = array[i];
-            array[i] = array[indexMin];
-            array[indexMin] = temp;
+            int temp = arr[i];
+            arr[i] = arr[indexMin];
+            arr[indexMin] = temp;
         }
     }
 
     public static void printArray() {
-        for (int i : array) {
+        for (int i : arr) {
             System.out.print(i + " ");
         }
         System.out.println();
