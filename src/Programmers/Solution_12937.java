bd31da31c511aa6e7ec7343a2ec47a39a36e5afc
이승민 <sml7179@naver.com>
2021-09-01 18:06:44 +0900

    프로그래머스 짝수와 홀수 문제풀이

---
+++ src/Programmers/Solution_12937.java
@@ -0,0 +1,10 @@
+package Programmers;
+
+//https://programmers.co.kr/learn/courses/30/lessons/12937
+public class Solution_12937 {
+    public String solution(int num) {
+
+        String answer = num%2==0?"Even":"Odd";
+        return answer;
+    }
+}
