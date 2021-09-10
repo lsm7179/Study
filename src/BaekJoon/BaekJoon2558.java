3ec73f118261103b5acec5db83d15a598bdb27cb
이승민 <sml7179@naver.com>
2021-09-10 17:50:15 +0900

    백준 A+B - 2 문제풀이

---
+++ src/BaekJoon/BaekJoon2558.java
@@ -0,0 +1,15 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BaekJoon2558 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        int m=Integer.parseInt(br.readLine());
+        System.out.println(n+m);
+
+    }
+
+}
