d9e45d91d3e677a50d470f50ad8de33c1ed77f9d
이승민 <sml7179@naver.com>
2021-09-12 17:33:41 +0900

    백준 16진수 문제풀이

---
+++ src/BaekJoon/sol_1000/BaekJoon1550.java
@@ -0,0 +1,14 @@
+package BaekJoon.sol_1000;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon1550 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String no=br.readLine();
+        System.out.print(Integer.parseInt(no,16));
+        
+    }
+}
