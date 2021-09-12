70489ef9746eea87124df3b5a54be7549feae8a9
이승민 <sml7179@naver.com>
2021-09-12 16:48:02 +0900

    백준 파티가 끝나고 난뒤 문제풀이 수정

--- src/BaekJoon/BaekJoon2845.java
+++ src/BaekJoon/BaekJoon2845.java
@@ -1,19 +1,23 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
-
+import java.io.InputStreamReader;
 public class BaekJoon2845 {
     public static void main(String[] args) throws IOException {
-         Scanner scanner = new Scanner(System.in);
-        int[] result = new int[5]; int p = scanner.nextInt();
-        int area = scanner.nextInt();
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int[] result = new int[5];
+        String []pArea=br.readLine().split(" ");
+        int p = Integer.parseInt(pArea[0]);
+        int area = Integer.parseInt(pArea[1]);
         int cnt = p * area;
+        String [] number=br.readLine().split(" ");
         for (int i = 0; i < 5; i++) {
-            int temp = scanner.nextInt();
+            int temp = Integer.parseInt(number[i]);
             result[i] = temp - cnt;
         } for (int i : result) {
             System.out.println(i);
         }
     }
 }
+
