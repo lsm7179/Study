67a35ae752a25773e32b65da6ce9421f53a3bf16
이승민 <sml7179@naver.com>
2021-09-14 12:40:07 +0900

    백준 웰컴 문제풀이

---
+++ src/BaekJoon/BaekJoon5337.java
@@ -0,0 +1,10 @@
+package BaekJoon;
+
+public class BaekJoon5337 {
+    public static void main(String[] args) throws Exception{
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\"+"|(/.|(_.(_)[ | )(/.");
+    }
+
+}
