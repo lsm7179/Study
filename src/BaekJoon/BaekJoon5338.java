88e9de132834d1f5840ae073960c7cca98a64343
이승민 <sml7179@naver.com>
2021-09-17 14:45:30 +0900

    백준 5338 마이크로소프트 로고 문제풀이

---
+++ src/BaekJoon/BaekJoon5338.java
@@ -0,0 +1,12 @@
+package BaekJoon;
+
+public class BaekJoon5338 {
+    public static void main(String[] args) throws Exception{
+        System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+
+}
