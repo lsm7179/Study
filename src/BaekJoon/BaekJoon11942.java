cdb06902798e4f14bd149bb69239bbfa1e927cb5
이승민 <sml7179@naver.com>
2021-09-19 13:30:01 +0900

    백준 고려대는 사랑입니다 문제풀이

---
+++ src/BaekJoon/BaekJoon11942.java
@@ -0,0 +1,9 @@
+package BaekJoon;
+
+import java.io.IOException;
+
+public class BaekJoon11942 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("고려대학교");
+    }
+}
