1025afa76d93e082757daf1c2bf64db2efcfca7a
이승민 <sml7179@naver.com>
2021-09-28 18:55:39 +0900

    백준 2440 별 찍기 - 3 문제풀이

---
+++ src/BaekJoon/sol_2000/BaekJoon2440.java
@@ -0,0 +1,19 @@
+package BaekJoon.sol_2000;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BaekJoon2440 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = n; i > 0; i--) {
+            for (int j = i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
