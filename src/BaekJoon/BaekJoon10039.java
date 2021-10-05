47fdfcb8efea4477394b96f86b20100b8e8d8849
이승민 <sml7179@naver.com>
2021-10-05 20:34:29 +0900

    백준 10039 평균 점수 문제풀이

---
+++ src/BaekJoon/BaekJoon10039.java
@@ -0,0 +1,19 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BaekJoon10039 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int n = Integer.parseInt(br.readLine());
+           sum+=n<40?40:n;
+        }
+        System.out.print(sum/5);
+
+    }
+
+}
