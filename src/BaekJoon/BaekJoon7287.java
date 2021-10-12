e684b3ec85352ea6fec293c3fa584de291bdbad2
이승민 <sml7179@naver.com>
2021-10-12 21:35:53 +0900

    백준 7287 등록 문제풀이

---
+++ src/BaekJoon/BaekJoon7287.java
@@ -0,0 +1,10 @@
+package BaekJoon;
+
+public class BaekJoon7287 {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(140);
+        System.out.println("lsm7179");
+    }
+
+}
