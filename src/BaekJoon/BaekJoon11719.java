245f409dcb409005770154a3524fe3c7a1912e83
이승민 <sml7179@naver.com>
2021-10-18 21:01:55 +0900

    백준 11719 그대로 출력하기 2 문제풀이

---
+++ src/BaekJoon/BaekJoon11719.java
@@ -0,0 +1,12 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BaekJoon11719 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.lines().forEach(System.out::println);
+
+    }
+}
