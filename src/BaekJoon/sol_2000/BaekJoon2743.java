376b30b251d236236d6c94476cba44ca49e02b00
이승민 <sml7179@naver.com>
2021-10-21 22:38:33 +0900

    백준 2743 단어 길이 재기 문제풀이

---
+++ src/BaekJoon/sol_2000/BaekJoon2743.java
@@ -0,0 +1,12 @@
+package BaekJoon.sol_2000;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BaekJoon2743 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(br.readLine().length());
+
+    }
+}
