6d13f95febc1a21f5be6b8d66a4c77512a97251e
이승민 <sml7179@naver.com>
2022-01-08 23:12:19 +0900

    feat: 큰수의법칙

--- src/Study/coding_test/큰수의법칙.java
+++ src/Study/coding_test/큰수의법칙.java
@@ -13,7 +13,10 @@ import java.util.stream.Collectors;
  * 입력 예시
  * 5 8 3
  * 2 4 5 4 6
- * */
+ * <p>
+ * 출력 예시
+ * 46
+ */
 public class 큰수의법칙 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,11 +29,21 @@ public class 큰수의법칙 {
         int m = input.get(1);
         int k = input.get(2);
 
-        List<Integer> data = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
-            data.add(Integer.parseInt(br.readLine()));
-        }
-        
+        List<Integer> data = Arrays.
+                stream(br.readLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
 
+        Collections.sort(data, Collections.reverseOrder());
+
+        int first = data.get(0);
+        int second = data.get(1);
+
+        // *
+        int count = (int) (m / (k + 1)) * k + m % (k + 1);
+
+        int result = count * first + (m - count) * second;
+
+        System.out.println(result);
     }
 }
