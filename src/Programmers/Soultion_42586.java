88262fa374728ae13e06f9818b342dc15de1c139
이승민 <sml7179@naver.com>
2022-10-14 00:08:16 +0900

    프로그래머스 기능개발 문제풀이

--- src/Programmers/Soultion_42586.java
+++ src/Programmers/Soultion_42586.java
@@ -14,49 +14,26 @@ public class Soultion_42586 {
     }
 
     public static int[] solution(int[] progresses, int[] speeds) {
-
         List<Integer> results = new ArrayList<>();
-        int result = 0;
-        int day = 0;
-        Queue<Integer> progressesQueue = new LinkedList<>();
-        Queue<Integer> speedsQueue = new LinkedList<>();
-
-        // 스택에 넣는다.
-        for(int i =0; i< progresses.length; i++){
-            progressesQueue.add(progresses[i]);
-            speedsQueue.add(speeds[i]);
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0; i<progresses.length; i++){
+            queue.add((int) (Math.ceil((100.0 - progresses[i]) / speeds[i])));
         }
 
-        while(!progressesQueue.isEmpty()){
-            // 스택에서 빼서 speeds를 추가하여 100%가 되게한다.(이때 int day로 날짜를 확인한다. 그다음 result +1한다.)
-            int progress = progressesQueue.poll();
-            int speed = speedsQueue.poll();
+        while (!queue.isEmpty()){
+            int day = queue.poll();
+            int cnt = 1;
 
-            if(day == 0){
-                day = (100 - progress)/speed;
-                result++;
-                // 다음 스택에서 day와 speed를 합쳐서 100%가 넘으면 result++ 한다.
-            }else if(progress + (day * speed) >= 100) {
-                result++;
-            }else {
-                // 100%가 안되면
-                // result 를 list에 담고 result를 초기화한다.
-                results.add(result);
-                result = 1;
-
-                //day를 계산해야함.
-                day += (100 - (progress + day * speed)) /speed;
+            while(!queue.isEmpty() && day >= queue.peek()){
+                cnt++;
+                queue.poll();
             }
-
+            results.add(cnt);
         }
-        // stack이 끝나면 마지막 result를 넣는다.
-        results.add(result);
 
-
-
-        int[] answer = {};
         return results.stream()
                 .mapToInt(Integer::intValue)
                 .toArray();
+
     }
 }
