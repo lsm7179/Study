7de27c5f542da48cc953a3c87d663101fc8fc786
승민 <sml7179@naver.com>
2024-11-11 18:33:49 +0900

    문제 추가

---
+++ src/Programmers/Solution_12900.java
@@ -0,0 +1,16 @@
+package Programmers;
+
+public class Solution_12900 {
+    // https://school.programmers.co.kr/learn/courses/30/lessons/12900
+    /*
+    가로 길이가 2이고 세로의 길이가 1인 직사각형모양의 타일이 있습니다.
+    이 직사각형 타일을 이용하여 세로의 길이가 2이고 가로의 길이가 n인 바닥을 가득 채우려고 합니다.
+    타일을 채울 때는 다음과 같이 2가지 방법이 있습니다.
+    타일을 가로로 배치 하는 경우
+    타일을 세로로 배치 하는 경우
+    예를들어서 n이 7인 직사각형은 다음과 같이 채울 수 있습니다.
+    * */
+    public static void main(String[] args) {
+
+    }
+}
