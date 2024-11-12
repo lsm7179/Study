76a2d45e6159ab2b23e961701b4182239af001af
승민 <sml7179@naver.com>
2024-11-12 19:17:57 +0900

    틀린문제 우선 커밋

--- src/Programmers/Solution_12900.java
+++ src/Programmers/Solution_12900.java
@@ -11,6 +11,20 @@ public class Solution_12900 {
     예를들어서 n이 7인 직사각형은 다음과 같이 채울 수 있습니다.
     * */
     public static void main(String[] args) {
-
+        // 1 2 3 4 5
+        // 1 2 3 5 8
+        int result = solution(4);
+        System.out.println(result);
     }
+    public static int solution(int n) {
+        //n을 2와 1로 조합 가능한 경우의수를 구하는것
+        int[] results = new int[n];
+        results[0] = 1;
+        results[1] = 2;
+        for (int i = 2; i <results.length; i++) {
+            results[i] = (results[i-1] + results[i-2]) % 1_000_000_007;
+        }
+        return results[n-1];
+    }
+
 }
