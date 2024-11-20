3c66e8a9e57a359728a3f688a3d98d28a80a92c8
승민 <sml7179@naver.com>
2024-11-20 18:36:13 +0900

    Solution_12904 풀이

--- src/Programmers/Solution_12904.java
+++ src/Programmers/Solution_12904.java
@@ -35,4 +35,37 @@ public class Solution_12904 {
         return 1;
     }
 
+    // gpt가 알려줌
+    public int solution2(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n]; // dp[i][j]는 s의 i부터 j까지가 팰린드롬인지 여부를 저장
+        int maxLength = 1; // 팰린드롬의 최소 길이는 1
+
+        // 모든 단일 문자는 팰린드롬
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        // 두 글자 팰린드롬 검사
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                maxLength = 2;
+            }
+        }
+
+        // 세 글자 이상에 대한 팰린드롬 검사
+        for (int len = 3; len <= n; len++) { // 부분 문자열 길이 len
+            for (int i = 0; i <= n - len; i++) { // 시작 인덱스 i
+                int j = i + len - 1; // 끝 인덱스 j
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    maxLength = len; // 가장 긴 팰린드롬 길이 갱신
+                }
+            }
+        }
+
+        return maxLength;
+    }
+
 }
