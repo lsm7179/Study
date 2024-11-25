e67f7218baafb9ea70c0353ed6203bdb840242ae
승민 <sml7179@naver.com>
2024-11-25 17:26:23 +0900

    가장 큰 정사각형 찾기

---
+++ src/Programmers/Solution_12905.java
@@ -0,0 +1,20 @@
+package Programmers;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12905
+// 가장 큰 정사각형 찾기
+public class Solution_12905 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int solution(int [][]board)
+    {
+        int answer = 1234;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
