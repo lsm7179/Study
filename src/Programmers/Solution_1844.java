180e7eae48256cd35c09f513353ee042183a7444
승민 <sml7179@naver.com>
2024-11-25 18:33:32 +0900

    게임 맵 최단거리

---
+++ src/Programmers/Solution_1844.java
@@ -0,0 +1,18 @@
+package Programmers;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java
+// 게임 맵 최단거리
+public class Solution_1844 {
+
+    //백트래킹으로 풀어야될듯함.
+    public static void main(String[] args) {
+        int[][] test = {{1, 1}, {1, 1}};
+        System.out.println(solution(test));
+    }
+
+    public static int solution(int[][] maps) {
+        int answer = 0;
+
+        return answer;
+    }
+}
